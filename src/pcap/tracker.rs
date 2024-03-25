70d9bd9483afe39a0953ee3cd2dc616b48aa0507
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2024-03-25 15:44:51 +0100

    do not deal with transport when processing ethernet packet

--- src/pcap/tracker.rs
+++ src/pcap/tracker.rs
@@ -1,7 +1,7 @@
 use std::{io, net::IpAddr};
 
 use anyhow::{bail, Result as AResult};
-use etherparse::{InternetSlice, Ipv4Slice, Ipv6Slice, SlicedPacket, TcpSlice, TransportSlice};
+use etherparse::{InternetSlice, IpNumber, Ipv4Slice, Ipv6Slice, SlicedPacket, TcpSlice};
 
 use crate::proxy::event::MapiEvent;
 
@@ -26,56 +26,43 @@ impl<'a> Tracker<'a> {
     /// Process the given packet as an Ethernet frame.
     pub fn process_ethernet(&mut self, data: &[u8]) -> AResult<()> {
         let ether_slice = SlicedPacket::from_ethernet(data)?;
-        let transport_slice = ether_slice.transport.as_ref();
         match &ether_slice.net {
-            Some(InternetSlice::Ipv4(inet4)) => self.handle_ipv4(inet4, transport_slice),
-            Some(InternetSlice::Ipv6(inet6)) => self.handle_ipv6(inet6, transport_slice),
+            Some(InternetSlice::Ipv4(inet4)) => self.handle_ipv4(inet4),
+            Some(InternetSlice::Ipv6(inet6)) => self.handle_ipv6(inet6),
             None => Ok(()),
         }
     }
 
     /// Examine IPv6 packet. If it's a TCP packet and not fragmented, hand it to [Self::handle_tcp]
-    pub fn handle_ipv6(
-        &mut self,
-        ipv6: &Ipv6Slice,
-        transport: Option<&TransportSlice>,
-    ) -> AResult<()> {
+    pub fn handle_ipv6(&mut self, ipv6: &Ipv6Slice) -> AResult<()> {
         if ipv6.is_payload_fragmented() {
             bail!("pcap file contains fragmented ipv6 packet, not supported");
         }
 
-        let tcp = match transport {
-            None => bail!("transport not found, expected this only with fragmented packets"),
-            Some(TransportSlice::Tcp(tcp)) => tcp,
-            _ => return Ok(()),
-        };
+        let Some(IpNumber::TCP) = ipv6.extensions().first_header() else { return Ok(()); };
+        let payload = ipv6.payload().payload;
+        let Ok(tcp) = TcpSlice::from_slice(payload) else { return Ok(()); };
 
         let header = &ipv6.header();
         let src = IpAddr::from(header.source_addr());
         let dest = IpAddr::from(header.destination_addr());
-        self.handle_tcp(src, dest, tcp)
+        self.handle_tcp(src, dest, &tcp)
     }
 
     /// Examine IPv4 packet. If it's a TCP packet and not fragmented, hand it to [Self::handle_tcp]
-    pub fn handle_ipv4(
-        &mut self,
-        ipv4: &Ipv4Slice,
-        transport: Option<&TransportSlice>,
-    ) -> AResult<()> {
+    pub fn handle_ipv4(&mut self, ipv4: &Ipv4Slice) -> AResult<()> {
         if ipv4.is_payload_fragmented() {
             bail!("pcap file contains fragmented ipv4 packet, not supported");
         }
 
-        let tcp = match transport {
-            None => bail!("transport not found, expected this only with fragmented packets"),
-            Some(TransportSlice::Tcp(tcp)) => tcp,
-            _ => return Ok(()),
-        };
+        let IpNumber::TCP = ipv4.payload_ip_number() else { return Ok(()) };
+        let payload = ipv4.payload().payload;
+        let Ok(tcp) = TcpSlice::from_slice(payload) else { return Ok(()); };
 
         let header = &ipv4.header();
         let src = IpAddr::from(header.source_addr());
         let dest = IpAddr::from(header.destination_addr());
-        self.handle_tcp(src, dest, tcp)
+        self.handle_tcp(src, dest, &tcp)
     }
 
     /// Called by [Self::handle_ipv4] and [Self::handle_ipv6] when they encounter TCP traffic
